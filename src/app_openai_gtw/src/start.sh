6d88967c1bc0bac24609fc58a03d9387e54700a2
Marc Gueury <marc.gueury@oracle.com>
2025-11-03 19:38:40 +0100

    v3.0

--- src/app_openai_gtw/src/start.sh
+++ src/app_openai_gtw/src/start.sh
@@ -2,9 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-curl -s -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ > /tmp/instance.json
-export TF_VAR_compartment_ocid=`cat /tmp/instance.json | jq -r .compartmentId`
-export TF_VAR_region=`cat /tmp/instance.json | jq -r .region`
+. ./env.sh
 
 cd OCI_GenAI_access_gateway
 source myenv/bin/activate
