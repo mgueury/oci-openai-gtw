4f83633e49606c354f677cf105bf89f6330e0f45
Marc Gueury <marc.gueury@oracle.com>
2025-11-04 10:46:06 +0100

    v3.0

--- src/app_openai_gtw/src/OCI_GenAI_access_gateway/app/api/models/adapter/request_adapter.py
+++ src/app_openai_gtw/src/OCI_GenAI_access_gateway/app/api/models/adapter/request_adapter.py
@@ -183,7 +183,7 @@ class ChatRequestAdapter:
             oci_chat_request.chat_history = chat_history
             oci_chat_request.preamble_override = preamble_override
             # logger.info( "--- oci_chat_request:" + pprint.pformat(oci_chat_request) )
-            oci_chat_request.max_tokens = 20000
+            oci_chat_request.max_tokens = 4000
 
             if chat_request.tools:
                 oci_chat_request.tools = ToolAdapter.ToolsDefinitionAdapter.to_cohere(chat_request.tools)
